bf966a2138095990c78cd7ec2eefc84fdfb38330
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-08 11:41:41 +0530

    cookie added

--- script.js
+++ script.js
@@ -1,138 +1,135 @@
 const cookieExist=document.cookie;
 const name1=cookieExist.split("=")
-console.log(name1.length);
+console.log("my lengt",name1.length);
 console.log(typeof(name1));
-if(name1.length===0){
+if(name1.length===1){
+    
      console.log("hi")
-}
-else{
-    "http://127.0.0.1:5500/dashboard.html"
-}
-console.log(name1)
-let userDetails=JSON.parse(localStorage.getItem("userDetails"))||[];
-function signupForm(){
+     console.log(name1)
+    let userDetails=JSON.parse(localStorage.getItem("userDetails"))||[];
+    function signupForm(){
 
-    document.getElementById("signup").style.display="block";
-    document.getElementById("login").style.display="none";
-}
-function signinForm(){
-    document.getElementById("signup").style.display="none";
-    document.getElementById("login").style.display="block";
-}
-function validateForm(event){
-    event.preventDefault();
-    const username=document.getElementById("username").value;
-    const email=document.getElementById("email").value;
-    const phoneNumber=document.getElementById("phonenumber").value;
-    const password=document.getElementById("password").value;
-    if (!usernameValid(username)){
-        alert("Name should contain atleast 3 letters");
-        return false;
+        document.getElementById("signup").style.display="block";
+        document.getElementById("login").style.display="none";
     }
-    if (!emailValid(email) || !emailNotExist(email)){
-        alert("invalid email");
-        return false;
+    function signinForm(){
+        document.getElementById("signup").style.display="none";
+        document.getElementById("login").style.display="block";
     }
-    if (!passwordValid(password)){
-        alert("password should contain atlest 8characters including one uppercase,number and special character");
-        return false;
+    function validateForm(event){
+        event.preventDefault();
+        const username=document.getElementById("username").value;
+        const email=document.getElementById("email").value;
+        const phoneNumber=document.getElementById("phonenumber").value;
+        const password=document.getElementById("password").value;
+        if (!usernameValid(username)){
+            alert("Name should contain atleast 3 letters");
+            return false;
+        }
+        if (!emailValid(email) || !emailNotExist(email)){
+            alert("invalid email");
+            return false;
+        }
+        if (!passwordValid(password)){
+            alert("password should contain atlest 8characters including one uppercase,number and special character");
+            return false;
+        }
+        if(!phoneNumberValid(phoneNumber) ||!phonenumNotExist(phoneNumber) ){
+            alert("phone number should contain 10 numbers")
+            return false;
+        }
+        addDeatails(username,email,phoneNumber,password);
+        return true;
     }
-    if(!phoneNumberValid(phoneNumber) ||!phonenumNotExist(phoneNumber) ){
-        alert("phone number should contain 10 numbers")
-        return false;
-    }
-    addDeatails(username,email,phoneNumber,password);
-    return true;
-}
 
-function validateDetails(event){
-    event.preventDefault();
-    const existingusername=document.getElementById("existingusername").value;
-    const existingPassword=document.getElementById("existingPassword").value;
-    const userExist=userDetails.filter(obj=>obj.userEmail===existingusername);
-    if (userExist[0].userPassword === existingPassword){
-        const user=userExist[0].userName;
-        const idString=userExist[0].id;
-        const userNameid="userName"+idString;
-        const cookieExist=document.cookie;
-        console.log(cookieExist);
+    function validateDetails(event){
+        event.preventDefault();
+        const existingusername=document.getElementById("existingusername").value;
+        const existingPassword=document.getElementById("existingPassword").value;
+        const userExist=userDetails.filter(obj=>obj.userEmail===existingusername);
+        if (userExist[0].userPassword === existingPassword){
+            const user=userExist[0].userName;
+            const idString=userExist[0].id;
+            const userNameid="userName"+idString;
+            const cookieExist=document.cookie;
+            console.log(cookieExist);
         // const name1=cookieExist.split("=")
         // document.cookie = name1[0] +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
         // const cookieExist=getCookie(userNameid);
         // if(cookieExist===null){
             setCookie(userNameid,user,1);
         // } 
-        window.location.href = "http://127.0.0.1:5500/dashboard.html";
+            window.location.href = "http://127.0.0.1:5500/dashboard.html";
         // console.log(todate);
         // const tomorrow = 
         // console.log("hi",tomorrow.toString())
         // document.cookie=`username:${user}; expires:${tomorrow};path/`;
         
+        }
+        else{
+            console.log("incorrect")
+        }
     }
-    else{
-        console.log("incorrect")
+    function usernameValid(username){
+        const usernamePattern=/^[a-zA-Z ]{3,30}$/;
+        console.log(username)
+        if(usernamePattern.test(username)){
+            console.log("ji")
+            return true;
+        }else{
+            return false;
+        }
     }
-}
-function usernameValid(username){
-    const usernamePattern=/^[a-zA-Z ]{3,30}$/;
-    console.log(username)
-    if(usernamePattern.test(username)){
-        console.log("ji")
-        return true;
-    }else{
-        return false;
+    function emailValid(email){
+        const emailPattern=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        console.log(email);
+        if(emailPattern.test(email)){
+            console.log("ji")
+            return true;
+        }else{
+            return false;
+        }
     }
-}
-function emailValid(email){
-    const emailPattern=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    console.log(email);
-    if(emailPattern.test(email)){
-        console.log("ji")
-        return true;
-    }else{
-        return false;
-    }
-}
-function passwordValid(password){
-    if (/[!@#$%^&*()_+{}\[\]:;<>,.?~\-]/.test(password) &&
-    /[A-Z]/.test(password) &&
-    /\d/.test(password)
-    ) {
-        return true;
-    }
-    else{
-        return false;
-    }
+    function passwordValid(password){
+        if (/[!@#$%^&*()_+{}\[\]:;<>,.?~\-]/.test(password) &&
+        /[A-Z]/.test(password) &&
+        /\d/.test(password)
+        ) {
+            return true;
+        }
+        else{
+            return false;
+        }
 
-}
-function phoneNumberValid(phoneNumber){
-    const numberPattern= /^\d{10}$/;
-    if (numberPattern.test(phoneNumber)){
-        return true;
-    }else{
-        return false;
     }
-}
-function emailNotExist(email){
-    const userExist=userDetails.findIndex(obj=>obj.userEmail===email)
-    if (userExist!==-1){
-        alert("email already exist")
-        return false;
+    function phoneNumberValid(phoneNumber){
+        const numberPattern= /^\d{10}$/;
+        if (numberPattern.test(phoneNumber)){
+            return true;
+        }else{
+            return false;
+        }
     }
-    else {
-        return true;
+    function emailNotExist(email){
+        const userExist=userDetails.findIndex(obj=>obj.userEmail===email)
+        if (userExist!==-1){
+            alert("email already exist")
+            return false;
+        }
+        else {
+            return true;
+        }
     }
-}
-function phonenumNotExist(phoneNumber){
-    const userExist=userDetails.findIndex(obj=>obj.userNumber===phoneNumber)
-    if (userExist!==-1){
-        alert("phonenumber already exist")
-        return false;
+    function phonenumNotExist(phoneNumber){
+        const userExist=userDetails.findIndex(obj=>obj.userNumber===phoneNumber)
+        if (userExist!==-1){
+            alert("phonenumber already exist")
+            return false;
+        }
+        else {
+            return true;
+        }
     }
-    else {
-        return true;
-    }
-}
 
 // function getCookie(userNameid){
 //     console.log("find my user",userNameid);
@@ -169,31 +166,36 @@ function phonenumNotExist(phoneNumber){
 //     //console.log(cookieArr.indexOf(userNameid));
 // }
 
-function setCookie(name,userNameid,dayslive){
-    const todate=new Date();
-    todate.setTime(todate.getTime()+(dayslive*24*60*60*1000));
-    let expiryDate="expires="+todate.toUTCString();
-    document.cookie=`${name}=${userNameid};${expiryDate}; path/`
-    console.log(document.cookie);
-}
+    function setCookie(name,userNameid,dayslive){
+        const todate=new Date();
+        todate.setTime(todate.getTime()+(dayslive*24*60*60*1000));
+        let expiryDate="expires="+todate.toUTCString();
+        document.cookie=`${name}=${userNameid};${expiryDate}; path/`
+        console.log(document.cookie);
+    }
 
-function addDeatails(username,email,phoneNumber,password){
+    function addDeatails(username,email,phoneNumber,password){
     // let userDetails=JSON.parse(localStorage.getItem("userDetails"))||[];
-    let id;
-    if (userDetails.length===0){
-        id=1
-    }else{
-        id=userDetails[userDetails.length-1].id+1
+        let id;
+        if (userDetails.length===0){
+            id=1
+        }else{
+            id=userDetails[userDetails.length-1].id+1
+        }   
+        newUser={
+            id:id,
+            userName:username,
+            userEmail:email,
+            userNumber:phoneNumber,
+            userPassword:password
+        }
+        userDetails.push(newUser);
+        localStorage.setItem("userDetails", JSON.stringify(userDetails));
+        console.log(userDetails);
+        signinForm();
     }
-    newUser={
-        id:id,
-        userName:username,
-        userEmail:email,
-        userNumber:phoneNumber,
-        userPassword:password
-    }
-    userDetails.push(newUser);
-    localStorage.setItem("userDetails", JSON.stringify(userDetails));
-    console.log(userDetails);
-    signinForm();
+}
+else{
+    console.log("at dashboard")
+    window.location.href = "http://127.0.0.1:5500/dashboard.html"
 }
