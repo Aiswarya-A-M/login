186f6b40063fd40fff1ef69143138b8155e64d98
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-09 11:20:40 +0530

    error changed

--- dashboard.js
+++ dashboard.js
@@ -15,7 +15,7 @@ console.log("to delete")
 function deleteCookie() {
   console.log("inside delete");
   document.cookie =
-    cookieDeatails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
+    cookieDetails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
   //window.location.href = "http://127.0.0.1:5500/index.html";
   window.location.href="/login/index.html";
     cookieDetails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
