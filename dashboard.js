098ad2684c62eb558ef251126425117b73df1df3
Aiswarya-A-M <148937898+Aiswarya-A-M@users.noreply.github.com>
2023-11-09 10:20:56 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -5,7 +5,7 @@ console.log(cookieDeatails[1]);
 if (!cookieDeatails[1]) {
   console.log("hloi");
   // window.location.href = "http://127.0.0.1:5500/index.html";
-  window.location.href="https://aiswarya-a-m.github.io/login/index.html";
+  window.location.href="/login/index.html";
 }
 
 document.querySelector("#nameOfUser").innerHTML = cookieDeatails[1];
@@ -15,6 +15,6 @@ function deleteCookie() {
   document.cookie =
     cookieDeatails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
   //window.location.href = "http://127.0.0.1:5500/index.html";
-  window.location.href="https://aiswarya-a-m.github.io/login/index.html";
+  window.location.href="/login/index.html";
   console.log("end of delete");
 }
