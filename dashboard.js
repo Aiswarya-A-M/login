a08e94ae63f716a39100c5e75f312f0ab87b267e
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-09 11:49:20 +0530

    error checking

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,7 @@
 const cookieExist = document.cookie;
 const cookieDetails = cookieExist.split("=");
 console.log("hi");
+console.log(cookieDetails);
 console.log(cookieDetails[1]);
 if (!cookieDetails[1]) {
   console.log("hloi");
@@ -12,8 +13,8 @@ document.querySelector("#nameOfUser").innerHTML = cookieDetails[1];
 console.log("to delete")
 function deleteCookie() {
   console.log("inside delete");
-  document.cookie =
-    cookieDetails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
-  window.location.href="/login/index.html";
+  document.cookie = cookieDetails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
+  console.log(document.cookie);
+  window.location.href="/login/index.html"; 
   console.log("end of delete");
 }
