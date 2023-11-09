f121f2f7f14e8d441266bb29707aafec600bfa89
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-09 12:36:13 +0530

    cookie deleted function changed

--- dashboard.js
+++ dashboard.js
@@ -13,11 +13,10 @@ if (!cookieDetails[1]) {
 document.querySelector("#nameOfUser").innerHTML = cookieDetails[1];
 console.log("to delete")
 function deleteCookie() {
-  // window.location.href="/login/index.html"; 
+  window.location.href="/login/index.html"; 
   console.log("inside delete");
   console.log("before delete",document.cookie);
   document.cookie = 'userName' + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
-  // document.cookie = cookieDetails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
   console.log("after delete",document.cookie);
   console.log("end of delete");
 }
