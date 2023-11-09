3bb8822d1c005e422afcd6c32b8b0fc2c09624fa
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-09 10:27:16 +0530

    link changed

--- dashboard.js
+++ dashboard.js
@@ -14,7 +14,6 @@ function deleteCookie() {
   console.log("inside delete");
   document.cookie =
     cookieDeatails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
-  // window.location.href = "http://127.0.0.1:5500/index.html";
   window.location.href="/login/index.html";
   console.log("end of delete");
 }
