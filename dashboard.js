ef94f5ce8d8d746dfbbcdfd06210c952960396a7
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-09 11:26:20 +0530

    typos error changed

--- dashboard.js
+++ dashboard.js
@@ -4,8 +4,6 @@ console.log("hi");
 console.log(cookieDetails[1]);
 if (!cookieDetails[1]) {
   console.log("hloi");
-  // window.location.href = "http://127.0.0.1:5500/index.html";
-  window.location.href="/login/index.html";
   window.location.href="/login/index.html";
   console.log("checking");
 }
@@ -15,9 +13,6 @@ console.log("to delete")
 function deleteCookie() {
   console.log("inside delete");
   document.cookie =
-    cookieDetails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
-  //window.location.href = "http://127.0.0.1:5500/index.html";
-  window.location.href="/login/index.html";
     cookieDetails[0] + "=; Path=/; Expires=Thu, 21 Feb 2001 00:00:01 GMT;";
   window.location.href="/login/index.html";
   console.log("end of delete");
